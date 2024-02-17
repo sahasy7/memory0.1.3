59b93ff324ae43bdda7dab4562870afabc4a1eb2
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-18 00:32:14 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,8 +35,8 @@ def find_match(input):
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
 
 def query_refiner(conversation, query):
-
-    response = openai.Completion.create(
+    rags = OpenAI()
+    response = rags.Completion.create(
     model="text-davinci-003",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0.7,
