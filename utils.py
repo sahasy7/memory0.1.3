ec0f9a4cd631ca7195cdf3ddd49995d0b7b27614
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-19 23:01:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,43 +1,21 @@
 from sentence_transformers import SentenceTransformer
+import pinecone
+import openai
 import streamlit as st
-import os 
-import qdrant_client
-from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Qdrant
-from openai import OpenAI
-from openai import AsyncOpenAI
-os.environ['OPENAI_API_KEY'] =st.secrets.openai_key
-client = OpenAI(
-  api_key=st.secrets.openai_key,  # this is also the default, it can be omitted
-)
-QDRANT_API_KEY = st.secrets.QDRANT_API_KEY
-QDRANT_HOST = st.secrets.QDRANT_HOST
+openai.api_key =st.secrets.openai_key
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
-embeddings = OpenAIEmbeddings()
-
-def load_db():
-    clients = qdrant_client.QdrantClient(
-        url=QDRANT_HOST,
-        api_key=QDRANT_API_KEY,
-    )
-    vector_store = Qdrant(
-        client = clients,
-        collection_name = "gsm_demo.0.0.4",
-        embeddings = embeddings
-    )
-    print("connection established !")
-    return vector_store
-
-vectore_stor = load_db()
+pinecone.init(api_key=st.secrets.pincone, environment='us-west-2-aws')
+index = pinecone.Index('gsm-demo')
 
 def find_match(input):
-    input_em = embeddings.encode(input).tolist()
-    result = vectore_stor.as_retriever(input_em, top_k=2, includeMetadata=True)
+    input_em = model.encode(input).tolist()
+    result = index.query(input_em, top_k=2, includeMetadata=True)
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
 
 def query_refiner(conversation, query):
-    rags = AsyncOpenAI()
-    response = rags.Completion.create(
+
+    response = openai.Completion.create(
     model="text-davinci-003",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0.7,
@@ -51,6 +29,10 @@ def query_refiner(conversation, query):
 def get_conversation_string():
     conversation_string = ""
     for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
         
         conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
         conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
