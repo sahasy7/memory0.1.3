196eed901f23051bbca788a757934dd7e6e6a646
sahasy7 <74914769+sahasy7@users.noreply.github.com>
2024-02-20 11:00:06 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,40 +1,42 @@
 from sentence_transformers import SentenceTransformer
 import pinecone
-from pinecone import Pinecone
 import openai
 import streamlit as st
-from openai import OpenAI
 import os
-openai.api_key =st.secrets.openai_key
-client = OpenAI(
-     os.environ("OPENAI_API_KEY") = st.secrets.openai_key
-)
+
+# Set OpenAI API key
+openai.api_key = st.secrets.openai_key
+
+# Initialize OpenAI client
+client = openai.OpenAI(os.environ["OPENAI_API_KEY"])
+
+# Initialize Sentence Transformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
-pc = Pinecone(api_key=st.secrets.pincone)
+
+# Initialize Pinecone
+pc = pinecone.Pinecone(api_key=st.secrets.pincone)
 index = pc.Index('gsm-demo')
 
 def find_match(input):
     input_em = model.encode(input).tolist()
-    result = index.query(input_em, top_k=2, includeMetadata=True)
-    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+    result = index.query(input_em, top_k=2, include_metadata=True)
+    return result['matches'][0]['metadata']['text'] + "\n" + result['matches'][1]['metadata']['text']
 
 def query_refiner(conversation, query):
-
     response = client.chat.completions.create(
-    model="text-davinci-003",
-    prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-    temperature=0.7,
-    max_tokens=256,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
+        model="text-davinci-003",
+        prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
     return response['choices'][0]['text']
 
 def get_conversation_string():
     conversation_string = ""
-    for i in range(len(st.session_state['responses'])-1):
-        
-        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    for i in range(len(st.session_state['responses']) - 1):
+        conversation_string += "Human: " + st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: " + st.session_state['responses'][i + 1] + "\n"
     return conversation_string
